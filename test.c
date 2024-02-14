55457e84fc43921d98a7809cea734abe8fbeffac
jafber <spam@jan-berndt.de>
2024-02-14 21:39:54 +0100

    added hss

--- test.c
+++ test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
     printf("hallo welt\n");
+    printf("%i args\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
     return 0;
 }
